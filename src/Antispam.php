2409938ade5d74d79cdf1a2cda2e677ef83a9e03
hedeqiang <laravel_code@163.com>
2020-09-08 15:30:18 +0800

    图片检测

--- src/Antispam.php
+++ src/Antispam.php
@@ -55,7 +55,7 @@ class Antispam
      * 文本离线结果获取
      * @return array
      */
-    public function callback()
+    public function textCallback()
     {
         $params['version'] = Url::TEXT_VERSION;
         $params = $this->baseParams($params);
@@ -68,7 +68,7 @@ class Antispam
      * @param array $feedbacks String(json数组) 参考：https://support.dun.163.com/documents/2018041901?docId=396075425773023232
      * @return array
      */
-    public function feedback(array $feedbacks)
+    public function textFeedback(array $feedbacks)
     {
         $params['feedbacks'] = json_encode($feedbacks);
         //return $params;
@@ -118,7 +118,7 @@ class Antispam
      * @param array $params 参考：https://support.dun.163.com/documents/2018041901?docId=428324742066655232
      * @return array
      */
-    public function query(array $params = [])
+    public function TextQuery(array $params = [])
     {
 //        $params = [
 //            'id' => '23223254',
@@ -134,6 +134,50 @@ class Antispam
         $params = $this->baseParams($params);
 
         return $this->post($this->buildEndpoint(Url::ENDPOINT_TEXT_KEYWORD_URL_VERSION,'keyword/query'),$params);
+    }
+
+    /**
+     * 图片在线检测
+     * @param array $images 基本参数 String(json数组)
+     * @param array $checkLabels String 数组 接口指定过检分类，可多选，过检分类列表：100：色情，110：性感低俗，200：广告，210：二维码，300：暴恐，400：违禁，500：涉政
+     * @param array $extras 业务参数
+     * @return array
+     */
+    public function imageScan(array $images,array $checkLabels = [],array $extras = [])
+    {
+        $params['version'] = Url::IMAGE_VERSION;
+        $params['images'] = json_encode($this->getTask($images));
+        $params = array_merge($params,$checkLabels,$extras);
+        $params = $this->baseParams($params);
+
+        return $this->post($this->buildEndpoint(Url::ENDPOINT_IMAGE_URL_VERSION,'image/check'),$params);
+    }
+
+    /**
+     * 图片离线结果获取
+     * @return array
+     */
+    public function imageCallback()
+    {
+        $params['version'] = Url::IMAGE_VERSION;
+        $params = $this->baseParams($params);
+
+        return $this->post($this->buildEndpoint(Url::ENDPOINT_TEXT_URL_VERSION,'image/callback/results'),$params);
+    }
+
+    /**
+     * 图片机器结果反馈接口
+     * @param array $feedbacks String(json数组) 参考：https://support.dun.163.com/documents/2018041901?docId=396075425773023232
+     * @return array
+     */
+    public function imageFeedback(array $feedbacks)
+    {
+        $params['feedbacks'] = json_encode($feedbacks);
+        //return $params;
+        $params['version'] = Url::IMAGE_VERSION;
+        $params = $this->baseParams($params);
+
+        return $this->post($this->buildEndpoint(Url::ENDPOINT_TEXT_FEEDBACK_URL_VERSION,'image/feedback'),$params);
     }
 
 
@@ -217,8 +261,8 @@ class Antispam
             if(is_array($v)){
                 foreach ($v as $key => $value) {
                     $arr[$key] = $value;
-                    $tasks[] = $arr;
                 }
+                $tasks[] = $arr;
             }
             else{
                 $arr[$k] = $v;
